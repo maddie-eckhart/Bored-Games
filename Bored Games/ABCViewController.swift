55e2f3a8d8ed26540fc9afc7b89e2792ee07dd6d
Madeline Eckhart <madelineaeckhart@gmail.com>
2018-05-09 14:32:42 -0400

    ghost and ABC template

---
+++ Bored Games/ABCViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ABCViewController.swift
+//  Bored Games
+//
+//  Created by Madeline Eckhart on 5/9/18.
+//  Copyright © 2018 MaddGaming. All rights reserved.
+//
+
+import UIKit
+
+class ABCViewController: UIViewController {
+
+    let list = ["",]
+    
+    @IBAction func btnGetCategory(_ sender: Any) {
+        
+    }
+    
+    @IBOutlet weak var txtCategory: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Bored Games/GhostViewController.swift
@@ -0,0 +1,44 @@
+//
+//  GhostViewController.swift
+//  Bored Games
+//
+//  Created by Madeline Eckhart on 5/9/18.
+//  Copyright © 2018 MaddGaming. All rights reserved.
+//
+
+import UIKit
+
+class GhostViewController: UIViewController {
+
+    var alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N",
+                 "O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
+    @IBAction func btnGenerator(_ sender: Any) {
+        let num = Int(arc4random_uniform(25))
+        txtLetter.text = alpha[num]
+    }
+    @IBOutlet weak var txtLetter: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
