905fe012536123a37b9745f1faaa3ad7d40d4686
Madeline Eckhart <madelineaeckhart@gmail.com>
2018-08-10 23:15:09 -0500

    trying collection view

---
+++ Bored Games/CollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  CollectionViewCell.swift
+//  Bored Games
+//
+//  Created by Madeline Eckhart on 8/10/18.
+//  Copyright © 2018 MaddGaming. All rights reserved.
+//
+
+import UIKit
+
+class CollectionViewCell: UICollectionViewCell {
+    
+}
